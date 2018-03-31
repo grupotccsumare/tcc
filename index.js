0141db0dcdb28d368f76fbb82c88d7e918328f4b
hmneton <hmneton@gmail.com>
2018-03-31 20:30:38 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+let mysql = require('mysql')
+let client = mysql.createConnection({
+    user: 'root',
+    password: '',
+    host: '127.0.0.1',
+    port: 3306
+})
+client.query('create database okjjjjjjjjjjjj')
