7d9fb8cd42151fd669d63e5801eda0dc62a32c90
hmneton <hmneton@gmail.com>
2018-03-31 18:04:17 -0300

    commit

---
+++ stream/ex.js
@@ -0,0 +1,25 @@
+// @ts-check
+/**
+ * @typedef {Object} Issue
+ * @param {number} a
+ * @param {number} b
+ * @return {number}
+ * 
+ */
+
+function t(a, b) {
+    return a + b
+}
+let d = t(1, 3)
+
+let express = require('express')
+let app = express()
+
+/**@type { {a: string} } */
+let issue = {a:'k'}
+ 
+
+/** @type { number[] } */
+
+let bem = [0]
+
