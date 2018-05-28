42d7200817409ab342669eddf81babd1841fbe1f
grupotccsumare <39594992+grupotccsumare@users.noreply.github.com>
2018-05-28 18:50:34 -0300

    botao funcoes e div operadores
    adicionei a parte de funcoes, ainda esta incompleta. 
    mudei algumas coisas na div de operadores()

--- TCC/public/acoes.js
+++ TCC/public/acoes.js
@@ -24,18 +24,38 @@ $(document).ready(function () {
     $("#div-condicional").show();
     $("#div-repeticao").hide();
     $("#div-declaracao").hide();
+    $("#div-funcao").hide();
   });
   $("#btn-repeticao").click(function () {
     $("#div-repeticao").show();
     $("#div-condicional").hide();
     $("#div-declaracao").hide();
+    $("#div-funcao").hide();
   });
 
   $("#btn-declaracao").click(function () {
     $("#div-declaracao").show();
     $("#div-condicional").hide();
     $("#div-repeticao").hide();
+    $("#div-funcao").hide();
+  });
+
+  $("#btn-funcao").click(function () {
+    $("#div-funcao").show();
+    $("#div-condicional").hide();
+    $("#div-repeticao").hide();
+    $("#div-declaracao").hide();
   });
 
 
+});
+
+/*div-operadores*/
+$(document).ready(function () {
+  $("#input-funcao").on("keyup", function () {
+      var value = $(this).val().toLowerCase();
+      $("#myTable tr").filter(function() {
+          $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+      });
+  });
 });
